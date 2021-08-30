ee85e341a562d9068f50565cd6f584833111474e
Koras02 <wjdgh0727@gmail.com>
2021-08-31 03:25:09 +0900

    1.2배포용

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import SingleMovie from './SingleMovie';
+import Radium from "radium";
 
 function App() {
+  const styles = {
+    wrapper: {
+      width: "100%",
+      height: "auto",
+      margin:"-20px auto",
+      padding:"10px 0px",
+      background:"#2d3635",
+      float:"left"
+    },
+    h1 : {
+       textAlign:"center",
+     
+    }
+  }
+
+
+  const [movies, setMovies] = useState([]);
+ 
+  function getMovies(page) {
+    axios({
+      method: "get",
+      // eslint-disable-next-line no-undef
+      url: `/3/discover/tv?api_key=7db8b1ffbba88aaa67068565d84fe99f`
+    }).then(response => {
+      setMovies(response.data.results ?? [])
+      console.log(response);
+    }).catch((error) =>{
+      console.log(error);
+    })
+  }
+
+  useEffect(() => {
+    getMovies();
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div style={styles.wrapper}>
+         <h1 style={styles.h1}>영화 목록</h1>
+          {movies.length > 0 && movies.map((movie, index) => (
+              <>
+              <SingleMovie key={movie.id} id={movie.id} media_type={'tv'}  movie={movie} {...movie} />
+             </>
+          ))}
+ 
+     </div>
+  
+     </>
   );
 }
 
-export default App;
+export default Radium(App);
--- client/src/SingleMovie.js
+++ client/src/SingleMovie.js
@@ -1,20 +1,34 @@
 import React from 'react'
- 
 import {img_300} from "./config";
+import Radium from "radium";
+ 
+const styles = {
+    handler: {
+        width: "100%",
+
+        display:"block",
+        overflow: "hidden",
+    },
+    poster: {
+   
+
+    }
+}
 
 
 const SingleMovie = ({movie,id,title,poster_path, media_type, first_air_date,release_date, original_title, name,original_name, backdrop_path, vote_average }) => {
     return (
-        <div classNamee="handler">
-            <div media_type={media_type} id={id}>
+        <div style={styles.handler}>
+            <div media_type={media_type} id={id} style={styles.poster}>
                 <div className="poster__container">
-                <img className={'poster'} src={poster_path ? `${img_300}/${poster_path}` : null}
+                <img styles={styles.poster} src={poster_path ? `${img_300}/${poster_path}` : null}
                              alt={`${title ? title : (original_title ? original_title : (name ? name : original_name))}`}/>
-
                 </div>
+                <h1>{movie.name}</h1>
+                
             </div>
         </div>
     )
 }
 
-export default SingleMovie
+export default Radium(SingleMovie);
